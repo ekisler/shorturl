975fab8694f961f04657f3c5fa75661ab47c3786
Emisael Kisler <ekisler@gmail.com>
2023-02-18 12:35:08 -0400

    Generate Token with JWT

--- api/models/User.js
+++ api/models/User.js
@@ -1,5 +1,7 @@
-import { Schema, model } from "mongoose";
-const userSchema = new Schema({
+import mongoose from "mongoose";
+import bcryptjs from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -10,9 +12,27 @@ const userSchema = new Schema({
   },
   password: {
     type: String,
-    required: true
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  try {
+    const salt = await bcryptjs.genSalt(10);
+    user.password = await bcryptjs.hash(user.password, salt);
+    next();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Falló el Hash de contraseñas");
   }
-})
+});
 
-export const User = model('user, userSchema')
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcryptjs.compare(candidatePassword, this.password);
+};
 
+export const User = mongoose.model("User", userSchema);
